2acd706169a20c73b0ee9debfe8d942a8e78dbd4
aish <aishnikam31@gmail.com>
2023-12-22 21:07:07 +0530

    signup form done

--- frontend/src/userProfile/SignUp.jsx
+++ frontend/src/userProfile/SignUp.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'; // Use solid icons for Font Awesome
+
+import './signup.css';
 
 const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div>
-      
+      <div className="signup">
+        <form action="">
+          <h2>Create Account</h2>
+          <input type="text" placeholder='First and last name' />
+          <input type="email" placeholder='Email' />
+          <div className="password-container">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              placeholder='Password'
+              style={{ height: '85%', width:'100%', fontSize: '16px', marginRight: '10px' }}
+            />
+            <div
+              className="Password-icon"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+            </div>
+          </div>
+          <input
+            type={showPassword ? 'text' : 'password'}
+            placeholder='Confirm Password'
+          />
+          <button>Sign Up</button>
+          <p>
+            Already have an account? <a href="#">Log in</a>
+          </p>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
