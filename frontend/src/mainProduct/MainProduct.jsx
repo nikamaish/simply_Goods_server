44f2d663697758b960ca19505c899afc6f6d08d4
aish <aishnikam31@gmail.com>
2023-12-23 22:54:35 +0530

    MainProduct and filter2

--- frontend/src/mainProduct/MainProduct.jsx
+++ frontend/src/mainProduct/MainProduct.jsx
@@ -1,59 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css'; // Import the styles for the slider
+import 'rc-slider/assets/index.css';
 import './mainProduct.css';
 import { useParams } from 'react-router-dom';
 
-const MainProduct = ({ applyFilters }) => {
+const MainProduct = () => {
   const [priceRange, setPriceRange] = useState([0, 1000]);
-  const { index} = useParams();
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  const electronics = [
+    {
+      id: 1,
+      name: 'Pendrives and SD cards',
+      img: 'https://purepng.com/public/uploads/large/purepng.com-sandisk-usb-flash-pen-driveelectronics-pen-drive-usb-9415246650490ar6r.png',
+      price: 289,
+    },
+    // Add more electronic products as needed
+  ];
+
+  const { index } = useParams();
+
+  useEffect(() => {
+    // Filter products when the priceRange changes
+    const filtered = electronics.filter((product) => {
+      const productPrice = product.price;
+      return productPrice >= priceRange[0] && productPrice <= priceRange[1];
+    });
+
+    setFilteredProducts(filtered);
+  }, [priceRange]);
 
   const handlePriceChange = (range) => {
-  //   // Update the state and pass the selected price range to the parent component
     setPriceRange(range);
-    applyFilters({ price: range });
   };
-  // const MainProduct = ({ location }) => {
-  //   const { product } = location.state || {};
-    
-  //   // Use the product information for filtering or other purposes
-  //   // ...
-  // };
 
   return (
-
     <div className="sidebar">
       <h2>Filters</h2>
       <div>
-        <label>Filter 1:</label>
-        <select onChange={(e) => handlePriceChange('filter1', e.target.value)}>
-          {/* Options for Filter 1 */}
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-        </select>
-      </div>
-      <div>
-        <label>Filter 2:</label>
-        <select onChange={(e) => handlePriceChange('filter2', e.target.value)}>
-          {/* Options for Filter 2 */}
-          <option value="optionA">Option A</option>
-          <option value="optionB">Option B</option>
-        </select>
-      </div>
-      <div>
         <label>Price Range:</label>
-        <Slider
-          range
-          min={0}
-          max={1000}
-          value={priceRange}
-          onChange={handlePriceChange}
-        />
+        <Slider range min={0} max={1000} value={priceRange} onChange={handlePriceChange} />
         <p>
           Price: ₹ {priceRange[0]} - ₹ {priceRange[1]}
         </p>
       </div>
-      {/* Add more filters as needed */}
+
+      <div>
+        <h2>Filtered Products</h2>
+        {filteredProducts.map((product) => (
+          <div key={product.id}>
+            <p>{product.name}</p>
+            <p>Price: ₹{product.price}</p>
+            {/* Add other product details as needed */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
