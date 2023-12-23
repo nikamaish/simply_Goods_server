b752336dfd28efd1e03b12205e982be1fae491f1
aish <aishnikam31@gmail.com>
2023-12-23 13:16:07 +0530

    change in signup

--- frontend/src/userProfile/SignUp.jsx
+++ frontend/src/userProfile/SignUp.jsx
@@ -7,23 +7,33 @@ import './signup.css';
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [password, setPassword] = useState('');
+  // This is the password state which will be used to store the password value entered by the user. 
+  // first it is empty after that it stores first password that is entered by user.
+  
   const [confirmPassword, setConfirmPassword] = useState('');
   const [passwordMatch, setPasswordMatch] = useState(true);
 
-  const handlePasswordChange = (e) => {
-    const newPassword = e.target.value;
-    setPassword(newPassword);
 
-    // Check if the confirm password matches the new password
-    setPasswordMatch(confirmPassword === newPassword || confirmPassword === '');
+
+
+
+
+  const handlePasswordChange = (e) => {
+    // Extract the password entered by the user from the event object
+    const password = e.target.value;
+  
+    // Update the 'password' state with the entered password
+    setPassword(password); // old password
+
   };
+  
 
   const handleConfirmPasswordChange = (e) => {
     const newConfirmPassword = e.target.value;
     setConfirmPassword(newConfirmPassword);
 
     // Check if the confirm password matches the password
-    setPasswordMatch(password === newConfirmPassword || password === '');
+    setPasswordMatch(password === newConfirmPassword || password === ''); // new password and it is confirmed
   };
 
   return (
@@ -33,21 +43,29 @@ const SignUp = () => {
           <h2>Create Account</h2>
           <input type="text" placeholder='First and last name' />
           <input type="email" placeholder='Email' />
+
+
           <div className="password-container">
+            {/* This div element is used as a container to group the password input field and the visibility toggle icon/button. */}
             <input
+            // This is an <input> element of type "password" or "text" based on the showPassword state.
+            // here, the type attribute is set to a JavaScript expression which will be evaluated at runtime.
+            // here we use ternary operator to set the type attribute to "password" if showPassword is false, otherwise set it to "text".
               type={showPassword ? 'text' : 'password'}
               placeholder='Password'
               style={{ height: '85%', width: '100%', fontSize: '16px', marginRight: '10px' }}
               onChange={handlePasswordChange}
-              value={password}
-            />
-            <div
-              className="Password-icon"
-              onClick={() => setShowPassword(!showPassword)}
-            >
-              <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
-            </div>
+              value={password}/>
+
+
+<div className="Password-icon" onClick={() => setShowPassword(!showPassword)}>
+<FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+{/* The icon prop is set based on the showPassword state. If showPassword is true, it uses the "faEyeSlash" icon; otherwise, it uses the "faEye" icon. */}
+</div>
+
           </div>
+
+
           <input
             type={showPassword ? 'text' : 'password'}
             placeholder='Confirm Password'
