6b9fd61207c96d5b80415bd431e165edfa887741
aish <aishnikam31@gmail.com>
2024-01-03 02:34:41 +0530

    auth signup using jwt

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.get('/', (req, res) => {
+    res.send('It works!');
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect(process.env.MDB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+    console.error('Connection Error:', err.message);
+});
+
+db.once('open', () => {
+    console.log('Connected to the database');
+});
+
+app.use('/auth', require('./routers/userRouter'));
