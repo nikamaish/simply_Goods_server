196ab90a5971368a679c60b631b69964020d7772
aish <aishnikam31@gmail.com>
2024-01-03 13:28:49 +0530

    set-cookie is displayed

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -11,9 +11,13 @@ const generateRandomString = (length) => {
     return crypto.randomBytes(length).toString('hex');
   };
 
+// what is generateRandomString is that it is a function that generates a random string of a given length. So we are going to use this function to generate a random string that we are going to use as the session key. So what is a session key is that it is a string that is used to sign the session ID cookie. So what is a session ID cookie is that it is a cookie that is used to store the session ID. So what is a session ID is that it is a unique identifier that is used to identify the session. So what is a session is that it is a place where we can store data that we want to persist across requests. So what is a request is that it is a message that is sent from the client to the server.
 
   
 const sessionKey = generateRandomString(32); // You can adjust the length based on your security requirements
+// here we are generating a random string of length 32 and we are storing it in a variable called sessionKey
+// but sessionKey is used where? bcz i dont see it being used anywhere else in the code
+// in secret key, we are storing the session key that we generated in the previous step
 
 // console.log(sessionKey);
 
@@ -25,10 +29,19 @@ router.use(
       resave: false,
       saveUninitialized: false,
       cookie: {
-        secure: true,
+        // secure: true,
+        // when we hoste the app on heroku, we need to set secure to true
         httpOnly: true,
+        // what is httpOnly is that it is a security feature that prevents client side JavaScript from accessing the cookie. So if you are using React or Angular or Vue or any other front end framework, you will not be able to access the cookie from the front end. So this is a security feature that prevents cross-site scripting attacks.
+
         sameSite: 'none',
+        // sameSite is a security feature that prevents the browser from sending the cookie along with cross-site requests. So if you are on a website and that website is trying to make a request to your server, the browser will not send the cookie along with that request. So this is a security feature that prevents cross-site request forgery attacks.
+
+        // but then how server store user credentials if we set sameSite to none? 
+        // it is done by using a different cookie called csrf token. So what we do is that we set the sameSite to none and then we set another cookie called csrf token and we set the sameSite to strict for that cookie. So what happens is that the browser will send the csrf token cookie along with cross-site requests but it will not send the session cookie along with cross-site requests. So this is how we can store user credentials in the session and still prevent cross-site request forgery attacks.
+
         maxAge: 24 * 60 * 60 * 1000, // 1 day
+        // what is maxAge is that it is the time in milliseconds that the cookie will expire after
       },
     })
   );
@@ -53,10 +66,17 @@ router.use(
   
       const savedCustomer = await newCustomer.save();
   
-      req.session.user = { _id: savedCustomer._id, fullname: savedCustomer.fullname, email: savedCustomer.email };
+    //   req.session.user = { _id: savedCustomer._id, fullname: savedCustomer.fullname, email: savedCustomer.email };
   
+
+    req.session.user = Customer._id;
+// what we did over here is that we are storing the user id in the session and how it is different than above is that we are not storing the whole user object in the session, we are just storing the user id in the session and we are going to use that user id to fetch the user object from the database whenever we need it. So this is a more secure way of storing the user in the session.
+
       res.status(201).json(savedCustomer);
-    } catch (err) {
+    } 
+    
+    
+    catch (err) {
       console.error(err);
       res.status(500).send();
     }
@@ -77,6 +97,10 @@ router.use(
       }
   
       req.session.user = { _id: customer._id, fullname: customer.fullname, email: customer.email };
+    //   why we need to do it different bcz in register we only save the user id in the session but in login we are saving the whole user object in the session. So this is how we can store the user in the session.
+    // because we are going to use the user object in the session to display the user's name and email in the navbar. So this is why we need to store the whole user object in the session.
+    // it means when i login then then my name will be there in navbar and when i logout then my name will not be there in navbar is it right? yes
+
   
       res.status(200).json({ message: 'Login successful' });
     } catch (err) {
@@ -85,6 +109,7 @@ router.use(
     }
   });
   
+
   router.get('/logout', (req, res) => {
     try {
       req.session = null; // Clear the session data
