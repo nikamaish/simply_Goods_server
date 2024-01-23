a99caaf7c02616258ef134fb9495e323d0273a99
aish <aishnikam31@gmail.com>
2024-01-23 22:22:25 +0530

    changes in session and jwt

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,196 +1,152 @@
-const express = require('express');
-const router = express.Router();
-const session = require('express-session');
-const cookieSession = require('cookie-session');
-const Customer = require('../models/userModel');
+// routers help to create endpoints
+
+const router = require('express').Router();
 const bcrypt = require('bcryptjs');
-const crypto = require('crypto');
-require('dotenv').config();
+const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
-const MongoStore = require('connect-mongo')(session);
 
-const generateRandomString = (length) => {
-    return crypto.randomBytes(length).toString('hex');
-  };
+router.post('/register', async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
+    if (!email || !password )
+      return res
+        .status(400)
+        .json({ errorMessage: 'Please enter all required fields.' });
 
-  
-// what is generateRandomString is that it is a function that generates a random string of a given length. So we are going to use this function to generate a random string that we are going to use as the session key. So what is a session key is that it is a string that is used to sign the session ID cookie. So what is a session ID cookie is that it is a cookie that is used to store the session ID. So what is a session ID is that it is a unique identifier that is used to identify the session. So what is a session is that it is a place where we can store data that we want to persist across requests. So what is a request is that it is a message that is sent from the client to the server.
-
-  
-const sessionKey = generateRandomString(32); // You can adjust the length based on your security requirements
-// here we are generating a random string of length 32 and we are storing it in a variable called sessionKey
-// but sessionKey is used where? bcz i dont see it being used anywhere else in the code
-// in secret key, we are storing the session key that we generated in the previous step
-
-// console.log(sessionKey);
-
-
-
-router.use(
-    session({
-      secret: process.env.SECRET_KEY,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        secure: process.env.NODE_ENV === 'production',
-        // when we hoste the app on heroku, we need to set secure to true
-        httpOnly: true,
-        // what is httpOnly is that it is a security feature that prevents client side JavaScript from accessing the cookie. So if you are using React or Angular or Vue or any other front end framework, you will not be able to access the cookie from the front end. So this is a security feature that prevents cross-site scripting attacks.
-
-        sameSite: 'none',
-        // sameSite is a security feature that prevents the browser from sending the cookie along with cross-site requests. So if you are on a website and that website is trying to make a request to your server, the browser will not send the cookie along with that request. So this is a security feature that prevents cross-site request forgery attacks.
-
-        // but then how server store user credentials if we set sameSite to none? 
-        // it is done by using a different cookie called csrf token. So what we do is that we set the sameSite to none and then we set another cookie called csrf token and we set the sameSite to strict for that cookie. So what happens is that the browser will send the csrf token cookie along with cross-site requests but it will not send the session cookie along with cross-site requests. So this is how we can store user credentials in the session and still prevent cross-site request forgery attacks.
-
-        maxAge: 24 * 60 * 60 * 1000, // 1 day
-        // what is maxAge is that it is the time in milliseconds that the cookie will expire after
-
-      },
-      store: new MongoStore({ url: process.env.MDB_URL }),
-    })
-  );
-  
-
-  router.post('/register', async (req, res) => {
-    try {
-      const { email, password} = req.body;
-  
-      if (!email || !password ) {
-        return res.status(400).json({ errorMessage: 'Please enter all required fields.' });
-      }
-
-      if (password.length < 6)
+    if (password.length < 6)
       return res
         .status(400)
         .json({ errorMessage: 'Please enter a password of at least 6 characters.' });
 
-    // if (password !== passwordConfirm) {
-    //   return res
-    //     .status(400)
-    //     .json({ errorMessage: 'Please enter the same password twice.' });
-    // }
 
-  
-      const salt = await bcrypt.genSalt();
-      const passwordHash = await bcrypt.hash(password, salt);
-  
-      const newCustomer = new Customer({
-        // fullname,
-        email,
-        passwordHash
-      });
-  
-      const savedCustomer = await newCustomer.save();
-  
-    //   req.session.user = { _id: savedCustomer._id, fullname: savedCustomer.fullname, email: savedCustomer.email };
+
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser)
+      return res
+        .status(400)
+        .json({ errorMessage: 'An account with this email already exists.' });
+
+    // Hash the password
+    const salt = await bcrypt.genSalt();
+    // salt is a random string of characters
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // Create a new user with the hashed password
+    const newUser = new User({
+      email: email,
+      passwordHash: passwordHash,
+    });
+
+    // Save the new user to the database
+    const savedUser = await newUser.save();
+
   
     const token = jwt.sign({
-      user:savedCustomer._id
-  },process.env.JWT_SECRET);
+        user:savedUser._id
+    },process.env.JWT_SECRET);
 
-  console.log(token);  
-  res.cookie("token",token,{
-    httpOnly:true,
-    secure:process.env.NODE_ENV === 'production',
-    sameSite:"none"
-});
+    console.log(token);  
+    // it is a good practice to store the token in a cookie, cookie is a small piece of data sent from a website and stored on the user's computer by the user's web browser while the user is browsing.
+    // token is stored in a cookie so that the user can stay logged in even if they refresh the page or open a new tab but if the user closes the browser or the tab then the user will be logged out.
+    // the cookie is stored in the browser and the browser will send the cookie to the server with every request.
+    // if token stored in loal storage then it will be sent to the server with every request but it is not a good practice to store the token in local storage because it is vulnerable to cross site scripting attacks.
+    // if token stored in a cookie then it will be sent to the server with every request but it is not a good practice to store the token in a cookie because it is vulnerable to cross site request forgery attacks.
+    // if token stored in a http only cookie then it will be sent to the server with every request but it is not a good practice to store the token in a http only cookie because it is vulnerable to cross site scripting attacks.
+    // then where to store the token? we can store the token in a http only cookie with the secure flag set to true and the browser will only send the cookie to the server if the request is being made over https.
+    // http only cookie is a cookie that cannot be accessed by client side javascript code.
 
-  
-  req.session.user = savedCustomer._id;
-// what we did over here is that we are storing the user id in the session and how it is different than above is that we are not storing the whole user object in the session, we are just storing the user id in the session and we are going to use that user id to fetch the user object from the database whenever we need it. So this is a more secure way of storing the user in the session.
+    // send the token in a http only cookie
 
-      res.status(201).json({message: 'Customer registered successfully',savedCustomer});
-    } 
+    res.cookie("token",token,{
+        httpOnly:true,
+        secure:true,
+        sameSite:"none"
+    });
+
+
+    res.status(201).json(savedUser); // Respond with the saved user data or a success message
     
-    
-    catch (err) {
-      console.error(err);
-      res.status(500).send();
-    }
-  });
-  
-
-
-
-
-
-
-  router.post('/login', async (req, res) => {
-    try {
-      const { email, password } = req.body;
-  
-      if (!email || !password) {
-        return res.status(400).json({ errorMessage: 'Please enter both email and password.' });
-      }
-  
-      const existingCustomer = await Customer.findOne({ email });
-  
-      if (!existingCustomer){
-        return res.status(401).json({errorMessage:"Wrong email or password"});
-      }
-
-      const passwordCorrect = await bcrypt.compare(password, existingCustomer.passwordHash);
-
-      if(!passwordCorrect){
-        return res.status(401).json({errorMessage:"Wrong email or password"})
-      }
-
-      // req.session.user = { _id: existingCustomer._id, fullname: existingCustomer.fullname, email: existingCustomer.email };
-
-      req.session.user = { _id: existingCustomer._id, email: existingCustomer.email };
-
-    //   why we need to do it different bcz in register we only save the user id in the session but in login we are saving the whole user object in the session. So this is how we can store the user in the session.
-    // because we are going to use the user object in the session to display the user's name and email in the navbar. So this is why we need to store the whole user object in the session.
-    // it means when i login then then my name will be there in navbar and when i logout then my name will not be there in navbar is it right? yes
-
-    const token = jwt.sign({
-      user:existingCustomer._id
-  },process.env.JWT_SECRET);
-
-  res.cookie("token",token,{
-    httpOnly:true,
-    secure:process.env.NODE_ENV === 'production',
-    sameSite:"none"
-});
-  
-res.status(200).json({ message: "Customer logged in successfully", user: { id: existingCustomer._id, email: existingCustomer.email } });
-    }
-    
-    
-    catch (err) {
-      console.error(err);
-      res.status(500).send();
-    }
-  });
-  
-
-
-
-
-
-  router.get('/logout', (req, res) => {
-
-    try {
-      req.session = null; // Clear the session data
-
-      res.cookie('token', '', {
-          httpOnly: true,
-          expires: new Date(0),
-          domain: 'localhost', // Adjust as needed, especially if using subdomains
-      }).send();
-
-      res.status(200).json({ message: 'Logout successful' });
-      console.log('User session cleared');
-  }
-  
-  
+}  
   catch (err) {
-      console.error(err);
-      res.status(500).send();
-    }
-  });
-  
+    console.error(err);
+    res.status(500).send();
+  }
+});
 
-module.exports = router; 
+
+
+router.post('/login',async(req,res)=>{
+
+    try{
+
+        const{email, password}= req.body;
+        // it will take some time to find the user in the database so we will use await
+        // findOne() method will find the first document that matches the filter criteria
+        //  req.body is an object that contains the data submitted from the form
+
+        if(!email || !password){
+            return res.status(400).json({errorMessage:"Please enter all required fields."});
+            // 400 is a bad request status code 
+        }
+
+
+        const existingUser = await User.findOne({email:email});
+
+        if (!existingUser){
+            return res.status(401).json({errorMessage:"Wrong email or password."});
+            // 401 is an unauthorized status code
+        }
+
+        const passwordCorrect = await bcrypt.compare(password,existingUser.passwordHash);
+        // compare() method will compare the password entered by the user with the hashed password stored in the database
+        // what if dont write this line of code? then the user will be logged in even if the password is wrong
+        
+        if(!passwordCorrect){
+            return res.status(401).json({errorMessage:"Wrong email or password."});
+        }
+        const token = jwt.sign({
+            user:existingUser._id
+        },process.env.JWT_SECRET);
+
+
+        // send the token in a http only cookie
+        res.cookie("token",token,{
+          httpOnly:true,
+          secure:true,
+          sameSite:"none"
+        });
+      
+      
+        // res.status(201).json(savedUser); // Respond with the saved user data or a success message
+        res.status(200).json({ message: "User logged in successfully", user: { id: existingUser._id, email: existingUser.email } });
+
+        // does above line of code requried?
+    }
+    catch(err){
+        res.status(500).json({ errorMessage: 'Internal Server Error' });
+
+    }
+});
+
+
+
+router.get('/logout', (req, res) => {
+    res.cookie('token', '', {
+        httpOnly: true,
+        expires: new Date(0),
+        domain: 'localhost', // Add this line if needed
+    }).send();
+
+    // empty value? because we are deleting the cookie and not sending any data in the response body, we are just sending an empty cookie
+    // httpOnly:true? because we want to delete the cookie on the client side , send is used to send the cookie to the client
+    // why syntax is like this? because we are not sending any data in the response body
+    // new Date(0) is a date in the past, it expries the cookie immediately and the browser will delete the cookie but when the user press logout button then the user will be logged out immediately but the user will be logged out only on the client side, the user will still be logged in on the server side.
+    // we can also delete the cookie on the server side but we will not do that because it is not a good practice to store the token in a cookie on the server side.
+// but without entering related email and password, how can we logout?
+// we can logout by deleting the cookie on the client side
+});
+
+
+
+module.exports = router;
